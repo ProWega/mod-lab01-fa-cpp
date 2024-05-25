8c2e1cb195d27eed78780a7c4620b472a7c1a3f3
ProWega <deriswerderis@gmail.com>
2024-05-25 09:28:37 +0300

    Первое задание

--- src/fun.cpp
+++ src/fun.cpp
@@ -1,14 +1,41 @@
-// Copyright 2022 UNN-IASR
+// Copyright 2024 UNN-IASR
 #include "fun.h"
+#include <cstring>
+#include <cctype>
+unsigned int faStr1(const char* str) {
+    unsigned int count = 0;
+    bool inWord = false;
+    bool containsDigit = false;
 
-unsigned int faStr1(const char *str) {
+    while (*str != '\0') {
+        if (std::isspace(*str)) {
+            if (inWord && !containsDigit) {
+                ++count;
+            }
+            inWord = false;
+            containsDigit = false;
+        }
+        else {
+            if (std::isdigit(*str)) {
+                containsDigit = true;
+            }
+            inWord = true;
+        }
+        ++str;
+    }
+
+    // Проверка последнего слова, если оно было допустимым словом без цифр
+    if (inWord && !containsDigit) {
+        ++count;
+    }
+
+    return count;
+}
+
+unsigned int faStr2(const char* str) {
     return 0;
 }
 
-unsigned int faStr2(const char *str) {
-    return 0;
-}
-
-unsigned int faStr3(const char *str) {
+unsigned int faStr3(const char* str) {
     return 0;
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
-// Copyright 2022 UNN-IASR
+﻿// Copyright 2024 UNN-IASR
 #include "fun.h"
-
+#include <iostream>
 int main() {
+    std::cout << faStr1("H4ello, world!");
     return 0;
 }
